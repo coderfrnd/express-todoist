da9b468e3b3d81e3257566bdda8b297aa4046fcc
Abhishek <warriorsshadow39@gmail.com>
2025-03-14 14:32:01 +0530

    done

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 import express from "express";
 import { connectDb } from "./app/dataBaseConfig/db.config.js";
 import {
+  deleteAll,
   deleteById,
   getAllProject,
+  getById,
   projectCreate,
+  projectUpdateById,
 } from "./app/controllers/project.controller.js";
-import { createTask, getAllTask } from "./app/controllers/task.controller.js";
+import {
+  completedTask,
+  createTask,
+  deleteAllTaskById,
+  getAllTask,
+  updateTaskById,
+} from "./app/controllers/task.controller.js";
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -15,7 +24,13 @@ app.post("/", projectCreate);
 app.get("/", getAllProject);
 app.post("/task", createTask);
 app.get("/task", getAllTask);
-app.delete("/:id", deleteById);
+app.delete("/project/delete/:id", deleteById);
+app.post("/project/update/:id", projectUpdateById);
+app.get("/project/:id", getById);
+app.delete("/project", deleteAll);
+app.delete("/task/delete/:id", deleteAllTaskById);
+app.put("/task/update/:id", updateTaskById);
+app.get("/task/completed", completedTask);
 app.listen(port, () => {
   console.log("Port is running on", port);
 });
